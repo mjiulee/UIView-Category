e4feeeada5f75ed4ed7a5af5166e2a026dc5ee8d
zhangleo <zhangleowork@163.com>
2015-04-29 10:54:54 +0800

    add category.

---
+++ UIView+MBIBnspectable.h
@@ -0,0 +1,17 @@
+//
+//  UIView+MBIBnspectable.h
+//
+//  Created by 张磊 on 15/4/29.
+//  Copyright (c) 2015年 All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (MBIBnspectable)
+
+@property (assign,nonatomic) IBInspectable NSInteger cornerRadius;
+@property (assign,nonatomic) IBInspectable NSInteger borderWidth;
+@property (strong,nonatomic) IBInspectable UIColor *borderColor;
+@property (assign,nonatomic) IBInspectable BOOL onePx;
+
+@end
---
+++ UIView+MBIBnspectable.m
@@ -0,0 +1,49 @@
+//
+//  xiaomabao
+//
+//  Created by 张磊 on 15/4/29.
+//  Copyright (c) 2015年 All rights reserved.
+//
+
+#import "UIView+MBIBnspectable.h"
+
+@implementation UIView (MBIBnspectable)
+
+- (void)setCornerRadius:(NSInteger)cornerRadius{
+    self.layer.cornerRadius = cornerRadius;
+    self.layer.masksToBounds = cornerRadius > 0;
+}
+
+- (NSInteger)cornerRadius{
+    return self.layer.cornerRadius;
+}
+
+- (void)setBorderWidth:(NSInteger)borderWidth{
+    self.layer.borderWidth = borderWidth;
+}
+
+- (NSInteger)borderWidth{
+    return self.layer.borderWidth;
+}
+
+- (void)setBorderColor:(UIColor *)borderColor{
+    self.layer.borderColor = borderColor.CGColor;
+}
+
+- (UIColor *)borderColor{
+    return [UIColor colorWithCGColor:self.layer.borderColor];
+}
+
+- (void)setOnePx:(BOOL)onePx{
+    if (onePx) {
+        CGRect rect = self.frame;
+        rect.size.height = 1.0 / [UIScreen mainScreen].scale;
+        self.frame = rect;
+    }
+}
+
+- (BOOL)onePx{
+    return self.onePx;
+}
+
+@end
